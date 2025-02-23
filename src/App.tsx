33014497106cd2493026e1c2bd554457e2bdf04b
Sepehr Tavanaiee <iamsrte.pers@gmail.com>
2025-02-24 02:30:38 +0330

    Adding basic layout and shadcn components

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,20 @@
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from '@/components/ui/resizable.tsx';
 
 function App() {
-  return <div>weather-station</div>
+  return (
+    <ResizablePanelGroup
+      direction="horizontal"
+      className="w-full h-full flex-grow border-t"
+    >
+      <ResizablePanel defaultSize={25}></ResizablePanel>
+      <ResizableHandle withHandle />
+      <ResizablePanel defaultSize={75}></ResizablePanel>
+    </ResizablePanelGroup>
+  );
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ThemeProvider } from '@/components/ui/theme-provider';
 import App from './App.tsx';
+
+import './index.css';
+import 'maplibre-gl/dist/maplibre-gl.css';
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <App />
+        <ReactQueryDevtools />
+      </ThemeProvider>
+    </QueryClientProvider>
   </StrictMode>
 );
