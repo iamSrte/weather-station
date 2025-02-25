90a16b652e6391ba69443fa5751254ac9d8aac45
Sepehr Tavanaiee <iamsrte.pers@gmail.com>
2025-02-25 13:40:44 +0330

    Fixed a problem in geocoding custom hook

--- src/hooks/useGeocoding.ts
+++ src/hooks/useGeocoding.ts
@@ -1,27 +1,22 @@
 import { useCallback } from 'react';
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
-
-export interface Location {
-  name: string;
-  country: string;
-  state: string;
-  lat: number;
-  lon: number;
-}
+import { Location } from '@/components/map-view/store.ts';
 
 function useGeocoding(value: string) {
-  const fetchLocation = useCallback(() => {
-    return axios
-      .get<Location[]>('http://api.openweathermap.org/geo/1.0/direct', {
-        params: {
-          q: value.toUpperCase(),
-          limit: 5,
-          appid: import.meta.env.VITE_WEATHER_API_KEY,
-        },
-      })
-      .then((response) => response.data);
-  }, [value]);
+  const fetchLocation = useCallback(
+    () =>
+      axios
+        .get<Location[]>('http://api.openweathermap.org/geo/1.0/direct', {
+          params: {
+            q: value,
+            limit: 5,
+            appid: import.meta.env.VITE_WEATHER_API_KEY,
+          },
+        })
+        .then((response) => response.data),
+    [value]
+  );
 
   return useQuery<Location[], Error>({
     queryKey: ['searchLocation', value],
