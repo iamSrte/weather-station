89f4da37b429e5d7b8eee58598067b16da292286
Sepehr Tavanaiee <iamsrte.pers@gmail.com>
2025-03-30 13:08:33 +0330

    Make weather view layout responsive for mobile devices.

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,80 @@
+import { Drawer, DrawerContent, DrawerTitle, DrawerTrigger } from '@/components/ui/drawer';
 import {
   ResizableHandle,
   ResizablePanel,
   ResizablePanelGroup,
-} from '@/components/ui/resizable.tsx';
+} from '@/components/ui/resizable';
 import {
+  useMapStore,
   MaplibreMapView,
   OpenLayersMapView,
-  useMapStore,
 } from '@/components/map-view';
 import WeatherView from '@/components/weather-view';
 import NavigationBar from '@/components/navigation-bar';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const lat = useMapStore((state) => state.lat);
+  const lon = useMapStore((state) => state.lon);
   const map = useMapStore((state) => state.map);
+  const isSelected = useMapStore((state) => state.isSelected);
+
+  const [isOpen, setIsOpen] = useState(false)
+  const isDesktop = () => window.innerWidth >= 800;
+
+  const mapView =
+    map === 'MapLibre' ? (
+      <MaplibreMapView />
+    ) : map === 'OpenLayers' ? (
+      <OpenLayersMapView />
+    ) : (
+      'Map not found!'
+    );
+
+  useEffect(() => {
+    setIsOpen(isSelected);
+  }, [isSelected, lat, lon])
+
+  if (isDesktop()) {
+    return (
+      <>
+        <NavigationBar />
+        <ResizablePanelGroup
+          direction={'horizontal'}
+          className="w-full h-full flex-grow border-t"
+        >
+          <ResizablePanel
+            defaultSize={25}
+            minSize={17}
+            maxSize={30}
+            className="hidden md:flex md:flex-col"
+          >
+            <WeatherView />
+          </ResizablePanel>
+          <ResizableHandle withHandle className="hidden md:flex" />
+          <ResizablePanel defaultSize={75}>{mapView}</ResizablePanel>
+        </ResizablePanelGroup>
+      </>
+    );
+  }
 
   return (
     <>
       <NavigationBar />
-      <ResizablePanelGroup
-        direction="horizontal"
-        className="w-full h-full flex-grow border-t"
-      >
-        <ResizablePanel
-          defaultSize={20}
-          minSize={16}
-          maxSize={30}
-          className="hidden md:flex md:flex-col"
-        >
-          <WeatherView />
-        </ResizablePanel>
-        <ResizableHandle withHandle className="hidden md:flex" />
-        <ResizablePanel defaultSize={80}>
-          {map === 'MapLibre' ? (
-            <MaplibreMapView />
-          ) : map === 'OpenLayers' ? (
-            <OpenLayersMapView />
-          ) : (
-            'Map not found!'
-          )}
-        </ResizablePanel>
-      </ResizablePanelGroup>
+      {mapView}
+      <Drawer open={isOpen} onOpenChange={setIsOpen}>
+        <DrawerTitle hidden={true}>Weather View</DrawerTitle>
+        <DrawerTrigger asChild>
+          <div className="fixed py-2 rounded-t-md outline bg-background bottom-0 left-0 right-0 z-50 flex justify-center cursor-pointer">
+            <div className="h-2 w-25 rounded-full bg-muted-foreground/30 my-2" />
+          </div>
+        </DrawerTrigger>
+        <DrawerContent className="h-full max-h-[90vh] mb-2 pb-2">
+          <div className="h-full py-2">
+            <WeatherView />
+          </div>
+        </DrawerContent>
+      </Drawer>
     </>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { MapProvider } from 'react-map-gl/maplibre';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ThemeProvider } from '@/components/ui/theme-provider';
+import { Toaster } from '@/components/ui/sonner';
 import App from './App.tsx';
 
 import './index.css';
@@ -17,6 +18,7 @@ createRoot(document.getElementById('root')!).render(
       <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
         <MapProvider>
           <App />
+          <Toaster />
           <ReactQueryDevtools />
         </MapProvider>
       </ThemeProvider>
